7d1fe80f538f09dba66bfd64575576f9275fdf18
Laurin Quast <laurinquast@googlemail.com>
2021-06-05 23:49:43 +0200

    chore: fix test and setup ci for running test

--- src/define.ts
+++ src/define.ts
@@ -164,7 +164,7 @@ export type Factory<Ctx, TExtensionsMap extends ExtensionsMap > = {
     fields,
   }: {
     name?: string | undefined;
-    fields: Field<Ctx, RootSrc, any, {}>[];
+    fields: () => Field<Ctx, RootSrc, any, {}>[];
   }): ObjectType<Ctx, RootSrc>;
   mutationType<RootSrc>({
     name,
@@ -432,13 +432,13 @@ export function createTypesFactory<Ctx = undefined, TExtensions extends Extensio
       fields,
     }: {
       name?: string;
-      fields: Array<Field<Ctx, RootSrc, any>>;
+      fields: () => Array<Field<Ctx, RootSrc, any>>;
     }): ObjectType<Ctx, RootSrc> {
       return {
         kind: 'ObjectType',
         name,
         interfaces: [],
-        fieldsFn: () => fields,
+        fieldsFn: fields,
       };
     },
     mutationType<RootSrc>({
